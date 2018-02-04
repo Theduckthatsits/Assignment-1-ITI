3e2308cfcd84ee1632f053a3fda36c9c48d96bcb
saidghamra <35788699+saidghamra@users.noreply.github.com>
2018-02-04 14:50:04 -0500

    Add files via upload

--- Q2.3/Assignment.java
+++ Q2.3/Assignment.java
@@ -28,9 +28,40 @@ public class Assignment {
      */
     private static void setPlane(){
 
-		// your code goes there
+    // your code goes there
 
-	}
+          // Creating an instance of the class LinearRegression
+          LinearRegression linearRegression = new LinearRegression(2,2000);
+
+          // For loop used to add first 1000 points ((x, 2x), 5x)
+          for (int i=0; i<1000; i++) {
+
+               double[] temp = new double[]{1,i*1.0,i*2.0};
+
+               linearRegression.addSample(temp,5.0*i);
+            
+          }
+
+        // For loop used to add the other 1000 points ((2x, x), 4x)
+        for (int x=0; x<1000; x++) {
+
+          double[] temp = new double[]{1,x*2.0,x*1.0};
+
+          linearRegression.addSample(temp,4.0*x);
+            
+        }
+
+        // Gradient Descent Algorithm iterated 10,000 times in total
+        for (int z=0; z<10; z++) {
+
+          linearRegression.gradientDescent(0.000000003,1000);
+
+          // Printing out the curent hypothesis and cost
+          System.out.println("Hypothesis: "+linearRegression.currentHypothesis());
+          System.out.println("Cost: "+linearRegression.currentCost());
+            
+        }
+     }
 
 	/** 
      * In this second method, we will select a plane at random.
@@ -50,8 +81,47 @@ public class Assignment {
 
 	private static void randomPlane(){
 
-		// your code goes there
+          // your code goes there
 
+          // Creating an instance of the class LinearRegression
+          LinearRegression linearRegression = new LinearRegression(2,5000);
+
+          // Double a, b and c are randomly generated between -100 and +100
+          double a = generator.nextDouble()*200-100;
+          double b = generator.nextDouble()*200-100;
+          double c = generator.nextDouble()*200-100;
+
+          // For loop used to add 5000 sample points
+          for (int i=0; i<5000; i++) {
+
+               // Doubles x and y randomly generated between 50 and 4000
+               double x = generator.nextDouble()*3950+50;
+               double y = generator.nextDouble()*3950+50;
+
+               // double noise randomly generated between -20 and 20
+               double noise = generator.nextDouble()*40-20;
+
+               // Doubles temp and temp2 are used to hold the values of the sample point
+               double[] temp = new double[]{1,x,y};
+               double temp2 = (a*x)+(b*y)+c+noise;
+
+               // Adding the sample point to linearRegression
+               linearRegression.addSample(temp,temp2);
+               
+          }
+
+          // Gradient Descent Algorithm iterated  times in total
+          for (int z=0; z<10; z++) {
+
+               // Printing out the curent hypothesis, cost and target
+               System.out.println("Hypothesis: "+linearRegression.currentHypothesis());
+               System.out.println("Cost: "+linearRegression.currentCost());
+               System.out.println("Target: "+c+"x_0 + "+a+"x_1 + "+b+"x_2");
+               System.out.println();
+
+               // Gradient Descent
+               linearRegression.gradientDescent(0.000000003,1000);
+          }
 	}
 
 	/** 
